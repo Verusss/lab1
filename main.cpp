272ff9dc2ff720869a3785aab42c0de9185b32d7
Verusss <226327@student.pwr.edu.pl>
2016-04-20 08:12:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,54 @@
+/*Weronika Borek, 226327
+Funkcja wymaga podania dwóch argumentów wiersza poleceń - pierwszy z nich 
+określa rozmiar tablicy, drugi stanowi ziarno (seed value) dla działania funkcji srand().
+Program działa prawidłowo.
+*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-char **charSquare(int n);
-void drawCharSquare(char **square, int n);
+using namespace std;
 
-int main(int argc, char **argv)
+char **charSquare (int n);
+void drawCharSquare (char **square, int n);
+
+/*Program obsługuje błędy użytkownika, takie jak podanie nieprawidłowej ilości argmumentów wiersza poleceń, przetwarza je oraz przekazuje do dalszych funkcji.*/
+int main (int argc, char *argv[])
 {
-	int n;
-	drawCharSquare(charSquare(n), n);	
+  int n;
+  n=atoi(argv[1]);
+  srand (atoi(argv[2]));
+  if(argc!=3)
+    {
+      printf ("Niepoprawna liczba argumentow.\n");
+      return 0;
+    }
+  drawCharSquare(charSquare (n), n);
+  return 0;
 }
 
-char **charSquare(int n)
+/*Funkcja generuje tablice liczb pseudolosowych na podstawie ziarna oraz rozmiaru podanego w argumentach wiersza poleceń.*/
+char **charSquare (int n)
 {
-	char **square;
-	return square;
+  char **square=(char**)malloc(n*(sizeof(char*)));
+  for(int i=0; i<n; i++)
+    square[i]=(char*)malloc(n*(sizeof(char)));
+  for(int i=0; i<n*n; i++)
+    square[i/n][i%n]=rand()%26+97;
+  return square;
 }
 
-void drawCharSquare(char **square, int n)
+/*Funkcja wyświetla wygenerowaną wcześniej tablicę liczb.*/
+void drawCharSquare (char **square, int n)
 {
-
+  for (int i=0; i<n*n; i++)
+    {
+      if (i%n==n-1 && i<n*n-1)
+	printf ("%c\n", square[i/n][i%n]);
+      else
+	printf ("%c ", square[i/n][i%n]);
+    }
+  printf ("\n");
+  for (int i=0; i<n; i++)
+    free(square[i]);
+  free(square);
 }
